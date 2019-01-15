caa02f54d0f58778b010773cbf1f7fedb2bdfdd2
chriseteka <christopher.eteka.200986@unn.edu.ng>
2019-01-15 12:55:28 +0100

    Added an integration to microsoft bor framework using AZURE

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
+import { DirectLine } from "botframework-directlinejs";
+const directLine = new DirectLine({
+  secret: "cX2xv8Nr7lE.cwA.Ezg.M3TKmWXrVYOofcC0ZQoMohtB1npcJYfq9uG8Bpz9-Vc"
+});
+const botMessageToGiftedMessage = botMessage => ({
+  ...botMessage,
+  _id: botMessage.id,
+  createdAt: botMessage.timestamp,
+  user: {
+    _id: 2,
+    name: "Assistant_Dru",
+    avatar:
+      "https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-business-man-399587fe24739d5a-512x512.png"
+  }
+});
+function giftedMessageToBotMessage(message) {
+  return {
+    from: { id: 1, name: "The users_name" },
+    type: "message",
+    text: message.text
+  };
+}
 export default class App extends React.Component {
   state = {
-    messages: [
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date()
-      }
-    ]
+    messages: []
+  };
+constructor(props) {
+    super(props);
+    directLine.activity$.subscribe(botMessage => {
+      const newMessage = botMessageToGiftedMessage(botMessage);
+      this.setState({ messages: [newMessage, ...this.state.messages] });
+    });
+  }
+onSend = messages => {
+    this.setState({ messages: [...messages, ...this.state.messages] });
+    messages.forEach(message => {
+      directLine
+        .postActivity(giftedMessageToBotMessage(message))
+        .subscribe(() => console.log("success"), () => console.log("failed"));
+    });
   };
 render() {
     return (
       <View style={styles.container}>
-        <GiftedChat messages={this.state.messages} />
+        <GiftedChat
+          user={{
+            _id: 1
+          }}
+          messages={this.state.messages}
+          onSend={this.onSend}
+        />
       </View>
     );
   }
