36b34923fc22dfaf620eaca5ec00e8e45fea37a5
chriseteka <christopher.eteka.200986@unn.edu.ng>
2019-01-15 10:54:53 +0100

    Initial Commit to repo

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+export default class App extends React.Component {
+  state = {
+    messages: [
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date()
+      }
+    ]
+  };
+render() {
+    return (
+      <View style={styles.container}>
+        <GiftedChat messages={this.state.messages} />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
